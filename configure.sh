bdae2c9e889e6f7a46f80e689dfc4a5c1979c514
Chet Zema <chet.zema@cama.io>
2021-08-11 12:42:33 -0400

    Added configure.sh

---
+++ configure.sh
@@ -0,0 +1,15 @@
+cd deps/libft4222
+./install.sh
+cd examples
+cc get-version.c -lft4222 -Wl,-rpath,/usr/local/lib -o get-version
+./get-version
+echo -e "\033[1;31mShould have output something like \"Device 0: 'FT4222 A'\"\033[0m"
+cd ../..
+git clone https://github.com/jgarff/rpi_ws281x.git
+cd rpi_ws281x
+mkdir build
+cd build
+cmake ..
+cmake --build .
+sudo make install
+cd ../..
