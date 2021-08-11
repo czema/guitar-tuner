09d861be412b9db0bf9a42a35a48024dc9c5e84e
Chet Zema <chet.zema@cama.io>
2021-08-11 13:32:12 -0400

    Fix references.

--- configure.sh
+++ configure.sh
@@ -3,13 +3,15 @@ if [[ "$EUID" -ne 0 ]]; then
 	echo -e "\033[1;31mRoot access required.\033[0m" 2>&1
 	exit 1
 fi
+echo -e "\033[1;33mPreparing LibFT4222.\033[0m"
 cd deps/libft4222/
 ./install4222.sh
 cd examples
 cc get-version.c -lft4222 -Wl,-rpath,/usr/local/lib -o get-version
 ./get-version
-echo -e "\033[1;31mShould have output something like \"Device 0: 'FT4222 A'\"\033[0m"
+echo -e "\033[1;33mShould have output something like \"Device 0: 'FT4222 A'\"\033[0m"
 cd ../..
+echo -e "\033[1;33mPreparing rpi_ws281x.\033[0m"
 git clone https://github.com/jgarff/rpi_ws281x.git
 cd rpi_ws281x
 mkdir build
@@ -18,3 +20,4 @@ cmake ..
 cmake --build .
 sudo make install
 cd ../..
+echo -e "\033[1;32mConfiguration complete.\033[0m"
