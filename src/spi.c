951ae02db5a74c651505f3eb8da0c98bde76383d
Chet Zema <chet.zema@cama.io>
2021-08-25 10:42:08 -0400

    Share LED array to reduce copying

--- src/spi.c
+++ src/spi.c
@@ -203,9 +203,15 @@ int spi_reader(char *description) {
 		timeval_subtract(&diff, &stop, &start);
 
 		if (diff.tv_sec > 1 || diff.tv_usec > 30000) {
-			printf("%u.%u\n", diff.tv_sec, diff.tv_usec);
+			printf("P %u.%u\n", diff.tv_sec, diff.tv_usec);
+			gettimeofday(&start, NULL);
+			
 			display_render();
 			display_clear();
+			
+			gettimeofday(&stop, NULL);
+		timeval_subtract(&diff, &stop, &start);
+		printf("R %u.%u\n", diff.tv_sec, diff.tv_usec);
 			
 			gettimeofday(&start, NULL);
 		}
