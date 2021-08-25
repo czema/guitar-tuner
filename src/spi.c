fa80b703fa3bb5c00dec8241278eaa0c2fa11b51
Chet Zema <chet.zema@cama.io>
2021-08-25 10:31:37 -0400

    Share LED array to reduce copying

--- src/spi.c
+++ src/spi.c
@@ -253,7 +253,7 @@ int spi_reader(char *description) {
 
 				//if (diff.tv_sec > 1 || diff.tv_usec > 30000) {
 				if (diff.tv_sec > 1) {
-					puts("R");
+					puts("%u", diff.tv_sec);
 					display_render();
 					display_clear();
 					
