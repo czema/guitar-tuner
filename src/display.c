0dc3fadf0f687654402d5d382b77efe01c04d14f
Chet Zema <chet.zema@cama.io>
2021-08-25 10:27:30 -0400

    Share LED array to reduce copying

--- src/display.c
+++ src/display.c
@@ -16,7 +16,7 @@ This component interprets the Polytune LEDs state and converts it to a format su
 #define BLUE  0x000000F0
 
 // If the current LED is already on, leave it on.  Otherwise switch it based on row/col/color.
-#define SET(K, ROW, COL, COLOR) (LEDS[K] = LEDS[K] == 0 ? (ROW & COL) * COLOR : LEDS[K]; K++)
+#define SET(K, ROW, COL, COLOR) (LEDS[K] = LEDS[K] == 0 ? (ROW & COL) * COLOR : LEDS[K]); K++
 
 void display_update(uint8_t q1, uint8_t q2, uint8_t q3, uint8_t q4) {
 	if (q4 != 0) return; // If q4 doesn't hold all zeros then we are out of sync (the device transmits 32 bits but only has 3 HC595's).
