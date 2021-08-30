26cd1430d7d786ad1b9ff4faad67b29da6615fc4
Chet Zema <chet.zema@cama.io>
2021-08-30 14:57:34 -0400

    Decay LED brightness.

--- src/display.c
+++ src/display.c
@@ -18,7 +18,7 @@ This component interprets the Polytune LEDs state and converts it to a format su
 // If the current LED is already on, leave it on.  Otherwise switch it based on row/col/color.
 // Increment the LED by one each time it is encountered.
 //#define SET(K, ROW, COL, COLOR) (LEDS[K] = LEDS[K] == 0 ? (ROW & COL) * COLOR : LEDS[K]); K++
-#define SET(K, ROW, COL, COLOR) if (ROW & COL) LEDS[K]++; K++
+#define SET(K, ROW, COL, COLOR) if (ROW & COL) LEDS[K]++; else LEDS[K]--; K++
 
 void display_update(uint8_t q1, uint8_t q2, uint8_t q3, uint8_t q4) {
 	if (q4 != 0) return; // If q4 doesn't hold all zeros then we are out of sync (the device transmits 32 bits but only has 3 HC595's).
@@ -555,7 +555,9 @@ void display_update(uint8_t q1, uint8_t q2, uint8_t q3, uint8_t q4) {
 void display_reset(void) {
 	int i;
 	for (i = 0; i < LED_COUNT; i++) {
-		LEDS[i] = 0;
+		if (LEDS[i] < 0) {
+			LEDS[i] = 0;
+		}
 	}
 }
 
