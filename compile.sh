5fa0ecb88479bde75543c16725c138884a703a28
Chet Zema <chet.zema@cama.io>
2023-07-09 21:01:58 -0400

    Compile object files to a separate directory and do not delete them after compiling.

--- compile.sh
+++ compile.sh
@@ -5,12 +5,12 @@ if [[ "$EUID" -ne 0 ]]; then
 fi
 pushd src/
 echo -e "\033[1;33mCompiling...\033[0m"
-cc leds.c -c -Ofast -o leds.o
-cc display.c -c -Ofast -o display.o
-cc spi.c -c -Ofast -o spi.o
-cc main.c -c -Ofast -o main.o
+cc leds.c -c -Ofast -o ../obj/leds.o
+cc display.c -c -Ofast -o ../obj/display.o
+cc spi.c -c -Ofast -o ../obj/spi.o
+cc main.c -c -Ofast -o ../obj/main.o
+cd ../obj
 cc *.o -lft4222 -lws2811 -lm -Wl,-rpath,/usr/local/lib -Ofast -o ../bin/guitar-tuner
-rm *.o
 cd ../bin
 chown 0:0 guitar-tuner
 chmod u+s guitar-tuner
